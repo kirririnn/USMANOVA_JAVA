e5cbccffcc1de8ad84bd64854ab07877edabb7a2
Rin Toners <kirririnn@gmail.com>
2018-12-19 21:52:23 +0300

    add folders Task_01-02

---
+++ Tasks/Task_01/Program.java
@@ -0,0 +1,25 @@
+class Program {
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 0;
+		int a = 0;
+		int b = 1;
+		int c = 3;
+		int d = 1;
+		int f = 1;
+		int e = 1;
+		int s = 1;
+		System.out.println("15 867.00");
+		System.out.println("##########");
+		System.out.println("5000 - " + x);
+		System.out.println("2000 - " + y);
+		System.out.println("1000 - " + a);
+		System.out.println("500  - " + b);
+		System.out.println("100  - " + c);
+		System.out.println("50   - " + d);
+		System.out.println("10   - " + f);
+		System.out.println("5    - " + e);
+		System.out.println("2    - " + s);
+		
+	}
+}
---
+++ Tasks/Task_02/Program.java
@@ -0,0 +1,24 @@
+class Program {
+	public static void main (String[] args) {
+		int a = 137 % 2;
+		int b = 68 % 2;
+		int c = 34 % 2;
+		int d = 17 % 2;
+		int e = 8 % 2;
+		int f = 4 % 2;
+		int g = 2 % 2;
+		int h = 1 % 2;
+		System.out.println(" ******************");
+		System.out.println("    137 | 2 -> " + a);
+		System.out.println("    68  | 2 -> " + b);
+		System.out.println("    34  | 2 -> " + c);
+		System.out.println("    17  | 2 -> " + d);
+		System.out.println("    8   | 2 -> " + e);
+		System.out.println("    4   | 2 -> " + f);
+		System.out.println("    2   | 2 -> " + g);
+		System.out.println("    1   | 2 -> " + h);
+		System.out.println(" ******************");
+		System.out.println( "  "+ h +" "+ g +" "+ f +" "+ e +" "+ d +" "+ c +" "+ b +" " + a);
+		System.out.println(" ******************");
+	}
+}
